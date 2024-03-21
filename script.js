a0419c0fb799759dd88da0acd285f668ae7b1fda
Sofia29Gomes <anasofiacorreiagomes@gmail.com>
2024-03-21 16:58:01 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let slideIndex = 0;
+showSlide1();
+
+function showSlide1() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlide1, 1000); // Change image every 1 second
+}
+
